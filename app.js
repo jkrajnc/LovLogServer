49a2051964f25fc2cdd30cd8db9c4fdd1429e0d8
jkrajnc <jan.krajnc396@gmail.com>
2018-05-23 23:28:51 +0200

    Img<=>Base64 converter and REST

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const clanRouter = require('./routes/clan');
 const porociloRouter = require('./routes/porocilo');
 const aktivnostRouter = require('./routes/aktivnost');
 const koordinatePodrocjaRouter = require('./routes/koordinatePodrocja');
+const saveImage = require('./routes/saveImage');
 
 
 
@@ -36,6 +37,7 @@ app.use('/clani', clanRouter);
 app.use('/porocila', porociloRouter);
 app.use('/aktivnosti', aktivnostRouter);
 app.use('/koordinatePodrocja', koordinatePodrocjaRouter);
+app.use('/saveImage', saveImage);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
